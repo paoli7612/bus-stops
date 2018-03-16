2ba34c2a2959d91c0781daf6b5a702c321b6a66b
Tommaso Paoli <paoli7612@gmail.com>
2018-03-16 19:40:37 +0100

    get element by argv

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from bus import Bus
+from archive import Data
+class Program:
+    def __init__(self, city):
+        self.city = city
+        self.data = Data(self.city)
+        print(self.data)
 
-bus = Bus("1")
-print(bus)
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1: print("Immettere il nome della citta'")
+    elif len(sys.argv) > 2: print("Immettere solo una citta")
+    else: program = Program(sys.argv[1])
